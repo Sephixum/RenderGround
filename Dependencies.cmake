20e39577a97b6ad5680ff8bc494dac1b2308a8aa
Sephixum <emotive.5353@gmail.com>
2025-09-06 01:13:13 +0330

    Add SDL3 and glad as dependencies

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -13,22 +13,37 @@ function(project_setup_dependencies out_deps)
   set(local_deps "")
 
   if(NOT TARGET fmt::fmt)
-    cpmaddpackage("gh:fmtlib/fmt#11.1.4")
+    cpmaddpackage("gh:fmtlib/fmt#11.1.4") 
   endif()
   list(APPEND local_deps fmt::fmt)
 
   if(NOT TARGET spdlog::spdlog)
     cpmaddpackage(
-      NAME
-      spdlog
-      VERSION
-      1.15.3
-      GITHUB_REPOSITORY
-      "gabime/spdlog"
-      OPTIONS
-      "SPDLOG_FMT_EXTERNAL ON")
+      NAME spdlog
+      VERSION 1.15.3
+      GITHUB_REPOSITORY "gabime/spdlog"
+      OPTIONS "SPDLOG_FMT_EXTERNAL ON")
   endif()
   list(APPEND local_deps spdlog::spdlog)
+
+  if(NOT TARGET glad)
+    cpmaddpackage(
+      NAME glad
+      GITHUB_REPOSITORY "Sephixum/glad"
+      GIT_TAG master
+    )
+  endif()
+  list(APPEND local_deps glad)
+
+  if(NOT TARGET SDL3::SDL3)
+  cpmaddpackage(
+    NAME SDL3
+    GITHUB_REPOSITORY libsdl-org/SDL
+    GIT_TAG release-3.2.22
+  )
+  endif()
+  list(APPEND local_deps SDL3::SDL3)
+
 
   # Export the list to the called
   set(${out_deps} ${local_deps} PARENT_SCOPE)
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_video.h>
 #include <fmt/format.h>
+#include <glad/glad.h>
 #include <spdlog/spdlog.h>
 
 auto main() -> int {
-  fmt::println("I'm BATMAN");
-  spdlog::error("HEHE");
-  return 0;
+  fmt::println("I'M {}", "BATMAN");
+  spdlog::error("HE{}", "HE");
+  SDL_Init(SDL_INIT_VIDEO);
 }
