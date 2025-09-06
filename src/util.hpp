e349ae0fc3870e5596765d3555830c094b939754
Sephixum <emotive.5353@gmail.com>
2025-09-06 16:11:44 +0330

    Change ensure to use std::expected

--- src/util.hpp
+++ src/util.hpp
@@ -2,29 +2,64 @@
 
 #include <fmt/format.h>
 #include <source_location>
+#include <expected>
+
+using EnsureResult = std::expected<void, std::string>;
+using EnsureOk = EnsureResult;
+using EnsureError = std::unexpected<std::string>;
 
 template<typename... Ts>
-constexpr void ensure_true(bool predicate,
-                           fmt::format_string<Ts...> fmt_str,
-                           Ts &&...args,
-                           std::source_location source_loc = std::source_location::current()) {
+[[nodiscard]]
+constexpr auto
+    ensure_true(bool predicate,
+                fmt::format_string<Ts...> fmt_str,
+                Ts &&...args,
+                std::source_location source_loc = std::source_location::current()) noexcept
+    -> EnsureResult {
+    // TODO: Do not use excpetions
+    // throw std::runtime_error{fmt::format("\nfile: {}\nfunction: {}\nline: {}\nmessage: {}",
+    //                                      source_loc.file_name(),
+    //                                      source_loc.function_name(),
+    //                                      source_loc.line(),
+    //                                      formatted_message)};
+    // NOTE: done and changed to std::expected
+
+    // NOTE: usage would be :
+    // if (auto result = ensure_true(1>2)) {
+    //		log("it aight !");
+    // } else {
+    // 		log_error(result.error());
+    // 		std::exit(-1);
+    // }
+    //
+
     if(not predicate) {
-        // TODO: Do not use excpetions
         auto formatted_message = fmt::format(fmt_str, std::forward<Ts>(args)...);
-        throw std::runtime_error{fmt::format("\nfile: {}\nfunction: {}\nline: {}\nmessage: {}",
-                                             source_loc.file_name(),
-                                             source_loc.function_name(),
-                                             source_loc.line(),
-                                             formatted_message)};
+        auto formatted_error = fmt::format("\nmessage: {}\nfile: {}\nfunction: {}\nline: {}\n",
+                                           formatted_message,
+                                           source_loc.file_name(),
+                                           source_loc.function_name(),
+                                           source_loc.line());
+        return EnsureError{formatted_error};
     }
+
+    return EnsureOk{};
 }
 
-constexpr void ensure_true(bool predicate,
-                           std::source_location source_loc = std::source_location::current()) {
+[[nodiscard]]
+constexpr auto
+    ensure_true(bool predicate,
+                std::source_location source_loc = std::source_location::current()) noexcept
+    -> EnsureResult {
+    // NOTE: Same as the above
+
     if(not predicate) {
-        throw std::runtime_error{fmt::format("\nfile: {}\nfunction: {}\nline: {}",
-                                             source_loc.file_name(),
-                                             source_loc.function_name(),
-                                             source_loc.line())};
+        auto formatted_error = fmt::format("\nfile: {}\nfunction: {}\nline: {}",
+                                           source_loc.file_name(),
+                                           source_loc.function_name(),
+                                           source_loc.line());
+        return EnsureError{formatted_error};
     }
+
+    return EnsureOk{};
 }
