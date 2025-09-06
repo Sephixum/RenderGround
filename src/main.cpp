821caeffae7f546993570dfd70523e90dacf3f0a
Sephixum <emotive.5353@gmail.com>
2025-09-06 16:12:14 +0330

    Add simple ensure_true example in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_video.h>
 #include <fmt/format.h>
 #include <glad/glad.h>
 #include <spdlog/spdlog.h>
+#include "util.hpp"
 
 auto main() -> int {
-  fmt::println("I'M {}", "BATMAN");
-  spdlog::error("HE{}", "HE");
-  SDL_Init(SDL_INIT_VIDEO);
+    for(auto i : {1, 2, 3}) {
+        fmt::println("Hello");
+    }
+
+    if(auto res = ensure_true(1 > 2, "1 is not bigger than 2"); not res) {
+        spdlog::error(res.error());
+    }
+
+    fmt::println("I'M {}", "BATMAN");
+    spdlog::error("HE{}", "HE");
+    SDL_Init(SDL_INIT_VIDEO);
 }
