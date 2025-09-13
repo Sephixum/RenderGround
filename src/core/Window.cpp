1ea30f5e23873166365b51c6bd5f7dc241180742
Sephixum <emotive.5353@gmail.com>
2025-09-13 18:35:19 +0330

    Add window constructor and destructor

--- src/core/Window.cpp
+++ src/core/Window.cpp
@@ -1,5 +1,39 @@
 #include "Window.hpp"
+#include "SDL3/SDL_video.h"
+#include "util.hpp"
 
-Window::Window() {}
+Window::Window(Builder &&builder) {
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-Window::~Window() {}
+    m_window = SDLWindow{SDL_CreateWindow(builder.m_title.c_str(),
+                                          static_cast<int>(builder.m_width),
+                                          static_cast<int>(builder.m_height),
+                                          [&] {
+                                              auto flags = SDL_WindowFlags{};
+
+                                              if(builder.m_resizable)
+                                                  flags |= SDL_WINDOW_RESIZABLE;
+                                              if(builder.m_fullscreen)
+                                                  flags |= SDL_WINDOW_FULLSCREEN;
+                                              if(builder.m_borderless)
+                                                  flags |= SDL_WINDOW_BORDERLESS;
+                                              flags |= SDL_WINDOW_OPENGL;
+
+                                              return flags;
+                                          }())};
+    ensure_true(m_window != nullptr);
+
+    m_context = SDL_GL_CreateContext(m_window.get());
+    ensure_true(m_context != nullptr);
+    SDL_GL_MakeCurrent(m_window.get(), m_context);
+}
+
+Window::~Window() {
+    if(m_context) {
+        SDL_GL_DestroyContext(m_context);
+        m_context = nullptr;
+    }
+}
--- src/core/Window.hpp
+++ src/core/Window.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <SDL3/SDL.h>
+#include <cstdint>
+#include <string_view>
+#include <memory>
+
+using SDLWindow =
+    std::unique_ptr<SDL_Window,
+                    decltype([](auto window_ptr) static { SDL_DestroyWindow(window_ptr); })>;
 
 class Window final {
   public:
@@ -10,15 +17,36 @@ class Window final {
     static auto builder() -> Builder;
 
   private:
-    Window();
+    Window(Builder &&builder);
     Window(const Window &) = delete("Copying constructing a window is unreasonable.");
     auto operator=(const Window &) -> Window & = delete("Copy assigning a window is unreasonable.");
     Window(Window &&) = default;
     auto operator=(Window &&) -> Window & = default;
     ~Window();
 
-    SDL_Window *m_window = nullptr;
+    SDLWindow m_window = nullptr;
+    SDL_GLContext m_context = nullptr;
 };
 
-// Change to builder pattern
-class Window::Builder {};
+// TODO: Complete functions
+class Window::Builder final {
+    using Self = Window::Builder;
+
+  public:
+    auto setSize(this Self &&self, std::uint32_t width, std::uint32_t height) -> Self &&;
+    auto setPosition(this Self &&self, std::uint32_t x, std::uint32_t y) -> Self &&;
+    auto setTitle(this Self &&self, std::string_view title) -> Self &&;
+    auto resizeable(this Self &&self, bool value) -> Self &&;
+    auto fullscreen(this Self &&self, bool value) -> Self &&;
+    auto borderless(this Self &&self, bool value) -> Self &&;
+
+  private:
+    std::string m_title = "SDL3 Window";
+    std::uint32_t m_width = 800;
+    std::uint32_t m_height = 600;
+    bool m_resizable = true;
+    bool m_fullscreen = false;
+    bool m_borderless = false;
+
+    friend class Window;
+};
